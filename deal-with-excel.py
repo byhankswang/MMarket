1389a51cdfc070e2e0f37a3d3ca9107ed076d346
byhankswang <byhankswang@163.com>
2017-09-20 18:03:20 +0800

    Modify some code

--- deal-with-excel.py
+++ deal-with-excel.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*- 
+
+# Use python lib xlrd to deal with Microsoft Excel, 
+# Library for developers to extract data from Microsoft Excel (tm) spreadsheet files.
+# Which Public Website is https://pypi.python.org/pypi/xlrd
+
 import xdrlib, sys
 import xlrd
 
@@ -8,8 +13,9 @@ def open_excel(file= 'file.xls'):
         return data
     except Exception,e:
         print str(e)
-        
-#根据索引获取Excel表格中的数据   参数:file：Excel文件路径     colnameindex：表头列名所在行的所以  ，by_index：表的索引
+
+# 根据索引获取Excel表格中的数据
+# 参数:file：Excel文件路径     colnameindex：表头列名所在行的所以  ，by_index：表的索引
 def excel_table_byindex(file= '20170825_j1801.xls',colnameindex=0,by_index=0):
     data = open_excel(file)
     table = data.sheets()[by_index]
@@ -20,7 +26,6 @@ def excel_table_byindex(file= '20170825_j1801.xls',colnameindex=0,by_index=0):
     colnames =  table.row_values(colnameindex) #某一行数据 
     list =[]
     for rownum in range(1,nrows):
-
          row = table.row_values(rownum)
          if row:
              app = {}
@@ -29,7 +34,7 @@ def excel_table_byindex(file= '20170825_j1801.xls',colnameindex=0,by_index=0):
              list.append(app)
     return list
 
-#根据名称获取Excel表格中的数据   参数:file：Excel文件路径     colnameindex：表头列名所在行的所以  ，by_name：Sheet1名称
+# 根据名称获取Excel表格中的数据   参数:file：Excel文件路径     colnameindex：表头列名所在行的所以  ，by_name：Sheet1名称
 def excel_table_byname(file= '20170825_j1801.xls',colnameindex=0,by_name= '日成交持仓排名'):
     data = open_excel(file)
     table = data.sheet_by_name(by_name)
